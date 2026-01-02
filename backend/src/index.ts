3e4528dfb6f4b9350592cbe3433c9af0108b9d86
MrJujek <mr.jujek@gmail.com>
2026-01-02 19:52:01 +0100

    ✨ Add dev script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { register, login, refresh } from "./src/controllers/authController";
-import { AppDataSource } from "./src/config/data-source";
+import { register, login, refresh } from "./controllers/authController";
+import { AppDataSource } from "./config/data-source";
 
 dotenv.config();
 
