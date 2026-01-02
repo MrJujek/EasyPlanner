a50f59024089d4962536beba51ea40d35c3d9391
Radzi0slaw-102 <radek.klamka@gmail.com>
2026-01-02 19:48:47 +0100

    🐛 Moved index.ts content to main folder

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { register, login, refresh } from "./src/controllers/authController";
+import { AppDataSource } from "./src/config/data-source";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", register);
+app.post("/login", login);
+app.post("/refresh", refresh);
+
+AppDataSource.initialize()
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`EasyPlanner running on port ${PORT}`)
+        );
+    })
+    .catch((err) => console.log("Database connection error: ", err));
