a9277eb29d983639188973c4ff4287539c7bb81d
Julian Dworzycki <mr.jujek@gmail.com>
2026-01-03 19:11:53 +0100

    📂 Move AuthContext to contexts folder

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './AuthContextType';
+import { useAuth } from '../contexts/AuthContextType';
 
 export const ProtectedRoute = () => {
   const { token } = useAuth();
