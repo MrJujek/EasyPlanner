d73ea80b5126ddd4bee220292fab3171e778a5e2
Julian Dworzycki <mr.jujek@gmail.com>
2026-01-21 00:07:14 +0100

    🔨 Extract routes to separate files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,10 @@ import dotenv from "dotenv";
 import path from "path";
 import express from "express";
 import cors from "cors";
-import { register, login, refresh } from "./controllers/authController";
 import { AppDataSource } from "./config/data-source";
-import { authenticateToken } from "./middleware/authMiddleware";
-import {
-  createTask,
-  getTasks,
-  getSingleTask,
-  updateTask,
-  updateTaskParent,
-  toggleSubtasks,
-  deleteTask,
-  getTasksNoParents,
-} from "./controllers/taskController";
 import type { Request, Response, NextFunction } from "express";
+import authRoutes from "./routes/auth.routes";
+import taskRoutes from "./routes/task.routes";
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
@@ -31,20 +21,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", register);
-app.post("/login", login);
-app.post("/refresh", refresh);
-
-app.post("/tasks", authenticateToken, createTask);
-app.get("/tasks", authenticateToken, getTasks);
-app.get("/tasks/:id", authenticateToken, getSingleTask);
-app.put("/tasks/:id", authenticateToken, updateTask);
-app.patch("/tasks/:id/parent", authenticateToken, updateTaskParent);
-app.patch("/tasks/:id/subtasks", authenticateToken, toggleSubtasks);
-app.delete("/tasks/:id", authenticateToken, deleteTask);
-
-app.get("/subtasks", authenticateToken, getTasksNoParents);
-app.put("/subtasks/:id", authenticateToken, toggleSubtasks);
+app.use("/", authRoutes);
+app.use("/", taskRoutes);
 
 AppDataSource.initialize()
   .then(() => {
