544ec6ca6f03174e8cb994a6cf1cd32a1274ba72
MrJujek <mr.jujek@gmail.com>
2026-06-11 16:08:59 +0200

    🐛 Fix HeroUI path in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
